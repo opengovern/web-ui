19c7746295f0da28ce67c768f7fdf7606facad77
alidaghighi <daghighi1376@gmail.com>
2023-06-24 14:30:42 +0300

    changes in swiper region drawer

---
+++ src/pages/Assets/ResourceMetricsDetails/index.tsx
@@ -0,0 +1,13 @@
+import { useNavigation } from 'react-router-dom'
+
+export default function ResourceMetricsDetails() {
+    return (
+        <div>
+            <div className="flex justify-between gap-x-2">
+                <div className="flex flex-row justify-start items-start">
+                    details
+                </div>
+            </div>
+        </div>
+    )
+}
