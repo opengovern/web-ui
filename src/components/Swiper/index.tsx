de84df12e0b3bb4ef0e47469204a7be5b79a858c
alidaghighi <daghighi1376@gmail.com>
2023-06-24 14:30:42 +0300

    changes in swiper region drawer

--- src/components/Swiper/index.tsx
+++ src/components/Swiper/index.tsx
@@ -2,6 +2,7 @@ import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline'
 import { Button, Flex, Grid, GridProps } from '@tremor/react'
 import React, { Children, useMemo, useState } from 'react'
 import { cardContainer, pagination } from './styles'
+import Pagination from '../Pagination'
 
 interface IProps {
     children: React.ReactNode
@@ -23,92 +24,92 @@ const Swiper = ({
         [children]
     )
 
-    const [isShown, setIsShown] = useState(false)
-    const [isHover, setIsHover] = useState(false)
-    const showCarousel = (isHover || isShown) && itemsCount / pageSize > 1
+    // const [isShown, setIsShown] = useState(false)
+    // const [isHover, setIsHover] = useState(false)
+    // const showCarousel = (isHover || isShown) && itemsCount / pageSize > 1
 
-    const hoverEffect = {
-        opacity: showCarousel ? '100%' : '0',
-        transition: showCarousel ? 'all .3s ease' : 'all 3s ease',
-    }
+    // const hoverEffect = {
+    //     opacity: showCarousel ? '100%' : '0',
+    //     transition: showCarousel ? 'all .3s ease' : 'all 3s ease',
+    // }
 
-    const paginationDeactive = {
-        backgroundColor: '#C5C9D3',
-        ...hoverEffect,
-        ...pagination,
-    }
+    // const paginationDeactive = {
+    //     backgroundColor: '#C5C9D3',
+    //     ...hoverEffect,
+    //     ...pagination,
+    // }
+    //
+    // const paginationActive = {
+    //     backgroundColor: '#014BFA',
+    //     ...hoverEffect,
+    //     ...pagination,
+    // }
 
-    const paginationActive = {
-        backgroundColor: '#014BFA',
-        ...hoverEffect,
-        ...pagination,
-    }
+    // const previousButton = () => {
+    //     const preventPrevious = activeSlide === 1
+    //     return (
+    //         <Flex
+    //             alignItems="center"
+    //             justifyContent="center"
+    //             style={{
+    //                 position: 'absolute',
+    //                 top: '50%',
+    //                 width: '50px',
+    //                 height: '100%',
+    //                 transform: 'translateY(-50%)',
+    //                 left: '-50px',
+    //                 cursor: 'pointer',
+    //                 ...hoverEffect,
+    //             }}
+    //             onMouseEnter={() => setIsHover(true)}
+    //             onMouseLeave={() => setIsHover(false)}
+    //             onClick={() =>
+    //                 !preventPrevious && setActiveSlider((prev) => prev - 1)
+    //             }
+    //         >
+    //             <Button
+    //                 aria-label="previous"
+    //                 disabled={preventPrevious}
+    //                 size="xl"
+    //                 variant="light"
+    //                 icon={ChevronLeftIcon}
+    //             />
+    //         </Flex>
+    //     )
+    // }
 
-    const previousButton = () => {
-        const preventPrevious = activeSlide === 1
-        return (
-            <Flex
-                alignItems="center"
-                justifyContent="center"
-                style={{
-                    position: 'absolute',
-                    top: '50%',
-                    width: '50px',
-                    height: '100%',
-                    transform: 'translateY(-50%)',
-                    left: '-50px',
-                    cursor: 'pointer',
-                    ...hoverEffect,
-                }}
-                onMouseEnter={() => setIsHover(true)}
-                onMouseLeave={() => setIsHover(false)}
-                onClick={() =>
-                    !preventPrevious && setActiveSlider((prev) => prev - 1)
-                }
-            >
-                <Button
-                    aria-label="previous"
-                    disabled={preventPrevious}
-                    size="xl"
-                    variant="light"
-                    icon={ChevronLeftIcon}
-                />
-            </Flex>
-        )
-    }
-
-    const nextButton = () => {
-        const allowNext =
-            itemsCount > pageSize &&
-            Math.ceil(itemsCount / pageSize) > activeSlide
-        return (
-            <Flex
-                alignItems="center"
-                justifyContent="center"
-                style={{
-                    position: 'absolute',
-                    top: '50%',
-                    width: '50px',
-                    height: '100%',
-                    transform: 'translateY(-50%)',
-                    right: '-50px',
-                    cursor: 'pointer',
-                    ...hoverEffect,
-                }}
-                onMouseEnter={() => setIsHover(true)}
-                onMouseLeave={() => setIsHover(false)}
-                onClick={() => allowNext && setActiveSlider((prev) => prev + 1)}
-            >
-                <Button
-                    aria-label="previous"
-                    disabled={!allowNext}
-                    size="xl"
-                    variant="light"
-                    icon={ChevronRightIcon}
-                />
-            </Flex>
-        )
-    }
+    // const nextButton = () => {
+    //     const allowNext =
+    //         itemsCount > pageSize &&
+    //         Math.ceil(itemsCount / pageSize) > activeSlide
+    //     return (
+    //         <Flex
+    //             alignItems="center"
+    //             justifyContent="center"
+    //             style={{
+    //                 position: 'absolute',
+    //                 top: '50%',
+    //                 width: '50px',
+    //                 height: '100%',
+    //                 transform: 'translateY(-50%)',
+    //                 right: '-50px',
+    //                 cursor: 'pointer',
+    //                 ...hoverEffect,
+    //             }}
+    //             onMouseEnter={() => setIsHover(true)}
+    //             onMouseLeave={() => setIsHover(false)}
+    //             onClick={() => allowNext && setActiveSlider((prev) => prev + 1)}
+    //         >
+    //             <Button
+    //                 aria-label="previous"
+    //                 disabled={!allowNext}
+    //                 size="xl"
+    //                 variant="light"
+    //                 icon={ChevronRightIcon}
+    //             />
+    //         </Flex>
+    //     )
+    // }
 
     const renderItems = (slideNumber = 1) => {
         const items = Children.toArray(children).slice(
@@ -122,24 +123,43 @@ const Swiper = ({
     }
 
     // console.log(Math.ceil(itemsCount / pageSize), itemsCount, pageSize)
+    const handlePageChange = (page: number) => {
+        const pageCount = Math.ceil(itemsCount / pageSize)
+        if (activeSlide + page > pageCount) {
+            setActiveSlider(activeSlide)
+        } else if (activeSlide + page <= 0) {
+            setActiveSlider(1)
+        } else {
+            setActiveSlider(activeSlide + page)
+        }
+    }
 
     const paginationCount = () => {
-        const size: any = []
-        for (let i = 0; i < Math.ceil(itemsCount / pageSize); i += 1) {
-            size.push(
-                <button
-                    type="button"
-                    onClick={() => setActiveSlider(i + 1)}
-                    aria-label="page"
-                    style={
-                        activeSlide - 1 === i
-                            ? paginationActive
-                            : paginationDeactive
-                    }
-                />
-            )
-        }
-        return size
+        // const size: any = []
+        // for (let i = 0; i < Math.ceil(itemsCount / pageSize); i += 1) {
+        //     size.push(
+        //         <button
+        //             type="button"
+        //             onClick={() => setActiveSlider(i + 1)}
+        //             aria-label="page"
+        //             style={
+        //                 activeSlide - 1 === i
+        //                     ? paginationActive
+        //                     : paginationDeactive
+        //             }
+        //         />
+        //     )
+        // }
+        // return size
+        return (
+            <Pagination
+                onClickNext={() => handlePageChange(1)}
+                onClickPrevious={() => handlePageChange(-1)}
+                currentPage={activeSlide}
+                setCurrentPage={setActiveSlider}
+                pageCount={Math.ceil(itemsCount / pageSize)}
+            />
+        )
     }
 
     return (
@@ -148,22 +168,23 @@ const Swiper = ({
                 position: 'relative',
                 minHeight: '264px',
             }}
-            onMouseEnter={() => setIsShown(true)}
-            onMouseLeave={() => setIsShown(false)}
+            // onMouseEnter={() => setIsShown(true)}
+            // onMouseLeave={() => setIsShown(false)}
         >
             <span style={cardContainer}>
-                {previousButton()}
+                {/* {previousButton()} */}
                 <Grid {...gridContainerProps}>{renderItems(activeSlide)}</Grid>
-                {nextButton()}
+                {/* {nextButton()} */}
             </span>
-            <span
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                }}
-            >
-                {paginationCount()}
-            </span>
+            {/* <span */}
+            {/*    style={{ */}
+            {/*        display: 'flex', */}
+            {/*        justifyContent: 'center', */}
+            {/*    }} */}
+            {/* > */}
+            {/*    {paginationCount()} */}
+            {/* </span> */}
+            <div className="mt-7">{paginationCount()}</div>
         </span>
     )
 }
