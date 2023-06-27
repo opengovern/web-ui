7344799717755bd8a34b8331fd00bf2afb7cbb24
alidaghighi <daghighi1376@gmail.com>
2023-06-27 15:36:25 +0300

    fix

--- src/components/Cards/SummaryCard/index.tsx
+++ src/components/Cards/SummaryCard/index.tsx
@@ -77,6 +77,15 @@ export default function SummaryCard({
                     ) : (
                         <div className="text-sm text-blue-500 mt-4" />
                     )}
+                    <Button
+                        size="xs"
+                        variant="light"
+                        icon={ArrowLongRightIcon}
+                        iconPosition="right"
+                        onClick={onClick}
+                    >
+                        View more
+                    </Button>
                 </Flex>
             )}
         </Card>
