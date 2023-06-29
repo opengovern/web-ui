4403ef8e5e79d5437387639c9639ed4e085b3e48
alidaghighi <daghighi1376@gmail.com>
2023-06-29 15:02:46 +0300

    Fix assets

--- src/components/DrawerPanel/index.tsx
+++ src/components/DrawerPanel/index.tsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
-import { Divider } from '@tremor/react'
+import { Divider, Metric } from '@tremor/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 
 interface Iprops {
@@ -74,7 +74,7 @@ export default function DrawerPanel({
                                     <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
                                         <div className="px-4 sm:px-6">
                                             <Dialog.Title className="leading-6 text-gray-900 border-b pb-3">
-                                                {title}
+                                                <Metric>{title}</Metric>
                                             </Dialog.Title>
                                         </div>
                                         <div className="relative mt-6 flex-1 px-4 sm:px-6">
