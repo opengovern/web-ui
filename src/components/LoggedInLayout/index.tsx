897a45792a35b7383472d91b9c62f665f2795f4a
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-06-29 10:05:24 +0400

    fix: Fixes height

--- src/components/LoggedInLayout/index.tsx
+++ src/components/LoggedInLayout/index.tsx
@@ -212,9 +212,9 @@ export default function LoggedInLayout({
     )
 
     return (
-        <div>
+        <div className="h-screen">
             {showSidebar && sidebar}
-            <div className={showSidebar ? 'lg:pl-72' : ''}>
+            <div className={showSidebar ? 'lg:pl-72 h-full' : 'h-full'}>
                 <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-900 px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
                     <button
                         type="button"
