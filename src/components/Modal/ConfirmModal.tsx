62637f01bc9378c996ca426ca43cee5e0facbcf1
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-06-30 11:56:26 +0400

    fix: Adds confirm

---
+++ src/components/Modal/ConfirmModal.tsx
@@ -0,0 +1,74 @@
+import { Dialog } from '@headlessui/react'
+import { Button, Flex } from '@tremor/react'
+import { useState } from 'react'
+import Modal from '.'
+
+interface IConfirmModal {
+    open: boolean
+    onConfirm: () => void
+    onClose?: () => void
+    title?: string
+    description?: string
+}
+export default function ConfirmModal({
+    open,
+    onClose,
+    onConfirm,
+    title = 'Are you sure?',
+    description,
+}: IConfirmModal) {
+    const [loading, setLoading] = useState<boolean>(false)
+    return (
+        <Modal
+            open={open}
+            onClose={() => {
+                if (onClose) {
+                    onClose()
+                }
+            }}
+        >
+            <div>
+                <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title
+                        as="h3"
+                        className="text-base font-semibold leading-6 text-gray-900"
+                    >
+                        {title}
+                    </Dialog.Title>
+                    {description && (
+                        <div className="mt-2">
+                            <p className="text-sm text-gray-500">
+                                {description}
+                            </p>
+                        </div>
+                    )}
+                </div>
+            </div>
+            <Flex
+                justifyContent="between"
+                flexDirection="row"
+                className="mt-5 sm:mt-6"
+            >
+                <Button
+                    variant="secondary"
+                    loading={loading}
+                    className="w-1/2"
+                    onClick={onClose}
+                >
+                    No
+                </Button>
+                <Button
+                    variant="primary"
+                    loading={loading}
+                    className="w-1/2"
+                    onClick={() => {
+                        setLoading(true)
+                        onConfirm()
+                    }}
+                >
+                    Yes
+                </Button>
+            </Flex>
+        </Modal>
+    )
+}
--- src/pages/Settings/member_details.tsx
+++ src/pages/Settings/member_details.tsx
@@ -20,12 +20,14 @@ import {
 import Spinner from '../../components/Spinner'
 import DrawerPanel from '../../components/DrawerPanel'
 import { GithubComKaytuIoKaytuEnginePkgAuthApiWorkspaceRoleBinding } from '../../api/api'
+import ConfirmModal from '../../components/Modal/ConfirmModal'
 
 interface IMemberDetails {
     user?: GithubComKaytuIoKaytuEnginePkgAuthApiWorkspaceRoleBinding
     close: () => void
 }
 const MemberDetails: React.FC<IMemberDetails> = ({ user, close }) => {
+    const [deleteConfirmation, setDeleteConfirmation] = useState<boolean>(false)
     const [role, setRole] = useState<string>(user?.roleName || 'viewer')
     const [roleValue, setRoleValue] = useState<'viewer' | 'editor' | 'admin'>(
         'viewer'
@@ -112,81 +114,89 @@ const MemberDetails: React.FC<IMemberDetails> = ({ user, close }) => {
     ]
 
     return (
-        <List className="mt-4 h-full">
-            <ListItem key="title" className="py-4">
-                <Flex justifyContent="start" className="truncate space-x-4">
-                    <Text className="font-medium text-gray-800">
-                        Member Info
-                    </Text>
-                </Flex>
-            </ListItem>
-            {items.map((item) => {
-                return (
-                    <ListItem key={item.title} className="py-4">
-                        <Flex
-                            justifyContent="start"
-                            className="truncate space-x-4"
-                        >
-                            <Text className="font-medium text-gray-500">
-                                {item.title}
-                            </Text>
-                        </Flex>
-                        <Text className="text-gray-900">{item.value}</Text>
-                    </ListItem>
-                )
-            })}
-            <ListItem key="item" className="py-4">
-                <Flex
-                    justifyContent="between"
-                    alignItems="start"
-                    className="truncate space-x-4"
-                >
-                    <Text className="font-medium text-gray-500">Role</Text>
-
-                    <div className="space-y-5 sm:mt-0">
-                        {roleItems.map((item) => {
-                            return (
-                                <div className="relative flex items-start">
-                                    <div className="absolute flex h-6 items-center">
-                                        <input
-                                            name="roles"
-                                            type="radio"
-                                            className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                            onClick={() => {
-                                                setRole(item.value)
-                                            }}
-                                            checked={item.value === role}
-                                        />
-                                    </div>
-                                    <div className="pl-7 text-sm leading-6">
-                                        <div className="font-medium text-gray-900">
-                                            {item.title}
-                                        </div>
-                                        <p className="text-gray-500">
-                                            {item.description}
-                                        </p>
-                                    </div>
-                                </div>
-                            )
-                        })}
-                    </div>
-                </Flex>
-            </ListItem>
-            <ListItem key="buttons">
-                <Flex justifyContent="end" className="truncate space-x-4">
-                    <Button
-                        loading={loading}
-                        onClick={() => deleteRole()}
-                        variant="secondary"
+        <>
+            <ConfirmModal
+                title={`Are you sure you want to delete ${user.userName}?`}
+                open={deleteConfirmation}
+                onConfirm={deleteRole}
+                onClose={() => setDeleteConfirmation(false)}
+            />
+            <List className="mt-4 h-full">
+                <ListItem key="title" className="py-4">
+                    <Flex justifyContent="start" className="truncate space-x-4">
+                        <Text className="font-medium text-gray-800">
+                            Member Info
+                        </Text>
+                    </Flex>
+                </ListItem>
+                {items.map((item) => {
+                    return (
+                        <ListItem key={item.title} className="py-4">
+                            <Flex
+                                justifyContent="start"
+                                className="truncate space-x-4"
+                            >
+                                <Text className="font-medium text-gray-500">
+                                    {item.title}
+                                </Text>
+                            </Flex>
+                            <Text className="text-gray-900">{item.value}</Text>
+                        </ListItem>
+                    )
+                })}
+                <ListItem key="item" className="py-4">
+                    <Flex
+                        justifyContent="between"
+                        alignItems="start"
+                        className="truncate space-x-4"
                     >
-                        Delete
-                    </Button>
-                    <Button loading={loading} onClick={() => updateRole()}>
-                        Update Changes
-                    </Button>
-                </Flex>
-            </ListItem>
-        </List>
+                        <Text className="font-medium text-gray-500">Role</Text>
+
+                        <div className="space-y-5 sm:mt-0">
+                            {roleItems.map((item) => {
+                                return (
+                                    <div className="relative flex items-start">
+                                        <div className="absolute flex h-6 items-center">
+                                            <input
+                                                name="roles"
+                                                type="radio"
+                                                className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                                onClick={() => {
+                                                    setRole(item.value)
+                                                }}
+                                                checked={item.value === role}
+                                            />
+                                        </div>
+                                        <div className="pl-7 text-sm leading-6">
+                                            <div className="font-medium text-gray-900">
+                                                {item.title}
+                                            </div>
+                                            <p className="text-gray-500">
+                                                {item.description}
+                                            </p>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </Flex>
+                </ListItem>
+                <ListItem key="buttons">
+                    <Flex justifyContent="end" className="truncate space-x-4">
+                        <Button
+                            loading={loading}
+                            onClick={() => setDeleteConfirmation(true)}
+                            variant="secondary"
+                        >
+                            Delete
+                        </Button>
+                        <Button loading={loading} onClick={() => updateRole()}>
+                            Update Changes
+                        </Button>
+                    </Flex>
+                </ListItem>
+            </List>
+        </>
     )
 }
 
