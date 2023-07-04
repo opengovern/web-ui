47b2eedf98c063130ff67a0aa1ff19024111f15a
Kian AliAkbari <kiana@kaytu.io>
2023-07-04 18:54:29 +0330

    word changes in assets

--- src/pages/Assets/Details/ServicesDetails/Summary/index.tsx
+++ src/pages/Assets/Details/ServicesDetails/Summary/index.tsx
@@ -1,22 +1,12 @@
 import {
-    Card,
-    AccordionList,
-    Metric,
-    Text,
-    Flex,
-    ProgressBar,
-    Grid,
-    Icon,
-    Title,
-    Divider,
     Bold,
-    Accordion,
-    AccordionBody,
-    AccordionHeader,
-    BarList,
-    Color,
+    Card,
+    Flex,
     List,
     ListItem,
+    Metric,
+    Text,
+    Title,
 } from '@tremor/react'
 import React, { useEffect, useState } from 'react'
 import { numericDisplay } from '../../../../../utilities/numericDisplay'
@@ -174,7 +164,7 @@ export default function Summary({
             <div className="flex flex-row gap-x-10 mt-4">
                 <Card key="TopXServices" className="h-fit">
                     <Flex justifyContent="start" className="space-x-4">
-                        <Title className="truncate">Top Services</Title>
+                        <Title className="truncate">Popular Services</Title>
                     </Flex>
                     <List className="mt-2 mb-2">
                         {topServices.map((thing: any) => (
@@ -191,7 +181,9 @@ export default function Summary({
                 </Card>
                 <Card key="TopXFastest" className="h-fit">
                     <Flex justifyContent="start" className="space-x-4">
-                        <Title className="truncate">Top Growing Services</Title>
+                        <Title className="truncate">
+                            Top Fast-Growing Services
+                        </Title>
                     </Flex>
                     <List className="mt-2 mb-2">
                         {topFastest.map((thing: any) => (
