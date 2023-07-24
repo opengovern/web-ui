d7d2ab2128c611b09aab150abeba64710791b4df
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-07-24 17:34:31 +0400

    fix: Fixes environment variables

--- src/configs/globals.ts
+++ src/configs/globals.ts
@@ -4,9 +4,10 @@ declare global {
     interface Window {
         __RUNTIME_CONFIG__: {
             NODE_ENV: string
-            REACT_APP_CLIENT_ID: string
-            REACT_APP_DOMAIN: string
-            REACT_APP_AUDIENCE: string
+            RUNTIME_ENVIRONMENT: string
+            REACT_APP_AUTH0_CLIENT_ID: string
+            REACT_APP_AUTH0_DOMAIN: string
+            REACT_APP_AUTH0_AUDIENCE: string
             REACT_APP_BASE_URL: string
         }
     }
