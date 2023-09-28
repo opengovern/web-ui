5b0a485f3ecb624cc5c7e255613aae83704b0e2b
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-09-28 19:07:30 +0400

    fix: Fixes git repositories

--- src/pages/Settings/GitRepositories/index.tsx
+++ src/pages/Settings/GitRepositories/index.tsx
@@ -19,76 +19,10 @@ import { useComplianceApiV1QueriesSyncList } from '../../../api/compliance.gen'
 
 export default function SettingsGitRepositories() {
     const [updateInputs, setUpdateInputs] = useState<boolean>(false)
-    const [newAWSComplianceGitURL, setNewAWSComplianceGitURL] =
-        useState<string>('')
-    const [newAzureComplianceGitURL, setNewAzureComplianceGitURL] =
-        useState<string>('')
-    const [newInsightsGitURL, setNewInsightsGitURL] = useState<string>('')
-    const [newQueriesGitURL, setNewQueriesGitURL] = useState<string>('')
     const [newMetricsGitURL, setNewMetricsGitURL] = useState<string>('')
 
-    const {
-        response: awsComplianceGitURL,
-        isLoading: loadingAwsComplianceGitURL,
-    } = useMetadataApiV1MetadataDetail('aws_compliance_git_url')
-    const {
-        response: azureComplianceGitURL,
-        isLoading: loadingAzureComplianceGitURL,
-    } = useMetadataApiV1MetadataDetail('azure_compliance_git_url')
-    const { response: insightsGitURL, isLoading: loadingInsightGitURL } =
-        useMetadataApiV1MetadataDetail('insights_git_url')
-    const { response: queriesGitURL, isLoading: loadingQueriesGitURL } =
-        useMetadataApiV1MetadataDetail('queries_git_url')
     const { response: metricsGitURL, isLoading: loadingMetricsGitURL } =
         useMetadataApiV1MetadataDetail('analytics_git_url')
-    const {
-        isLoading: loadingSetAwsComplianceGitURL,
-        isExecuted: executeSetAwsComplianceGitURL,
-        sendNow: setAwsComplianceGitURL,
-    } = useMetadataApiV1MetadataCreate(
-        {
-            key: 'aws_compliance_git_url',
-            value: newAWSComplianceGitURL,
-        },
-        {},
-        false
-    )
-    const {
-        isLoading: loadingSetAzureComplianceGitURL,
-        isExecuted: executeSetAzureComplianceGitURL,
-        sendNow: setAzureComplianceGitURL,
-    } = useMetadataApiV1MetadataCreate(
-        {
-            key: 'azure_compliance_git_url',
-            value: newAzureComplianceGitURL,
-        },
-        {},
-        false
-    )
-    const {
-        isLoading: loadingSetInsightGitURL,
-        isExecuted: executeSetInsightGitURL,
-        sendNow: setInsightGitURL,
-    } = useMetadataApiV1MetadataCreate(
-        {
-            key: 'insights_git_url',
-            value: newInsightsGitURL,
-        },
-        {},
-        false
-    )
-    const {
-        isLoading: loadingSetQueriesGitURL,
-        isExecuted: executeSetQueriesGitURL,
-        sendNow: setQueriesGitURL,
-    } = useMetadataApiV1MetadataCreate(
-        {
-            key: 'queries_git_url',
-            value: newQueriesGitURL,
-        },
-        {},
-        false
-    )
     const {
         isLoading: loadingSetMetricsGitURL,
         isExecuted: executeSetMetricsGitURL,
@@ -107,13 +41,7 @@ export default function SettingsGitRepositories() {
         sendNow: syncQueries,
     } = useComplianceApiV1QueriesSyncList({}, false)
 
-    if (
-        loadingAwsComplianceGitURL ||
-        loadingAzureComplianceGitURL ||
-        loadingInsightGitURL ||
-        loadingQueriesGitURL ||
-        loadingMetricsGitURL
-    ) {
+    if (loadingMetricsGitURL) {
         return (
             <Flex justifyContent="center" className="mt-56">
                 <Spinner />
@@ -123,19 +51,11 @@ export default function SettingsGitRepositories() {
 
     if (!updateInputs) {
         setUpdateInputs(true)
-        setNewAWSComplianceGitURL(awsComplianceGitURL?.value || '')
-        setNewAzureComplianceGitURL(azureComplianceGitURL?.value || '')
-        setNewInsightsGitURL(insightsGitURL?.value || '')
-        setNewQueriesGitURL(queriesGitURL?.value || '')
         setNewMetricsGitURL(metricsGitURL?.value || '')
     }
 
     const save = async () => {
         const setUrls = [
-            setAwsComplianceGitURL(),
-            setAzureComplianceGitURL(),
-            setInsightGitURL(),
-            setQueriesGitURL(),
             setMetricsGitURL(),
         ]
         await Promise.all(setUrls).then(() => {
@@ -145,11 +65,6 @@ export default function SettingsGitRepositories() {
 
     const saveLoading = () => {
         return (
-            (executeSetAwsComplianceGitURL && loadingSetAwsComplianceGitURL) ||
-            (executeSetAzureComplianceGitURL &&
-                loadingSetAzureComplianceGitURL) ||
-            (executeSetInsightGitURL && loadingSetInsightGitURL) ||
-            (executeSetQueriesGitURL && loadingSetQueriesGitURL) ||
             (executeSetMetricsGitURL && loadingSetMetricsGitURL) ||
             (executeSyncQueries && loadingSyncQueries)
         )
@@ -167,102 +82,6 @@ export default function SettingsGitRepositories() {
                 </div>
             </Flex>
             <List className="mt-4">
-                <ListItem key="aws_compliance_git_url" className="my-1">
-                    <Flex justifyContent="start" className="truncate space-x-4">
-                        <div className="truncate">
-                            <Text className="truncate text-sm">
-                                AWS Compliance Git URL:
-                            </Text>
-                        </div>
-                    </Flex>
-                    <TextInput
-                        className="text-sm"
-                        value={newAWSComplianceGitURL}
-                        onChange={(e) =>
-                            setNewAWSComplianceGitURL(e.target.value)
-                        }
-                        icon={
-                            executeSetAwsComplianceGitURL &&
-                            loadingSetAwsComplianceGitURL
-                                ? Spinner
-                                : undefined
-                        }
-                        disabled={
-                            executeSetAwsComplianceGitURL &&
-                            loadingSetAwsComplianceGitURL
-                        }
-                    />
-                </ListItem>
-                <ListItem key="azure_compliance_git_url" className="my-1">
-                    <Flex justifyContent="start" className="truncate space-x-4">
-                        <div className="truncate">
-                            <Text className="truncate text-sm">
-                                Azure Compliance Git URL:
-                            </Text>
-                        </div>
-                    </Flex>
-                    <TextInput
-                        className="text-sm"
-                        value={newAzureComplianceGitURL}
-                        onChange={(e) =>
-                            setNewAzureComplianceGitURL(e.target.value)
-                        }
-                        icon={
-                            executeSetAzureComplianceGitURL &&
-                            loadingSetAzureComplianceGitURL
-                                ? Spinner
-                                : undefined
-                        }
-                        disabled={
-                            executeSetAzureComplianceGitURL &&
-                            loadingSetAzureComplianceGitURL
-                        }
-                    />
-                </ListItem>
-                <ListItem key="insights_git_url" className="my-1">
-                    <Flex justifyContent="start" className="truncate space-x-4">
-                        <div className="truncate">
-                            <Text className="truncate text-sm">
-                                Insights Git URL:
-                            </Text>
-                        </div>
-                    </Flex>
-                    <TextInput
-                        className="text-sm"
-                        value={newInsightsGitURL}
-                        onChange={(e) => setNewInsightsGitURL(e.target.value)}
-                        icon={
-                            executeSetInsightGitURL && loadingSetInsightGitURL
-                                ? Spinner
-                                : undefined
-                        }
-                        disabled={
-                            executeSetInsightGitURL && loadingSetInsightGitURL
-                        }
-                    />
-                </ListItem>
-                <ListItem key="queries_git_url" className="my-1">
-                    <Flex justifyContent="start" className="truncate space-x-4">
-                        <div className="truncate">
-                            <Text className="truncate text-sm">
-                                Queries Git URL:
-                            </Text>
-                        </div>
-                    </Flex>
-                    <TextInput
-                        className="text-sm"
-                        value={newQueriesGitURL}
-                        onChange={(e) => setNewQueriesGitURL(e.target.value)}
-                        icon={
-                            executeSetQueriesGitURL && loadingSetQueriesGitURL
-                                ? Spinner
-                                : undefined
-                        }
-                        disabled={
-                            executeSetQueriesGitURL && loadingSetQueriesGitURL
-                        }
-                    />
-                </ListItem>
                 <ListItem key="metrics_git_url" className="my-1">
                     <Flex justifyContent="start" className="truncate space-x-4">
                         <div className="truncate">
