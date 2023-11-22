512d6e0bf505bab478ed43adb31c43440b3d33ac
Kian AliAkbari <kiana@kaytu.io>
2023-11-22 18:36:46 +0330

    rc fix

--- src/pages/ResourceCollection/Detail/index.tsx
+++ src/pages/ResourceCollection/Detail/index.tsx
@@ -189,68 +189,63 @@ export default function ResourceCollectionDetail() {
                         alignItems="start"
                         className="h-full"
                     >
-                        <>
-                            <Title className="font-semibold mb-2">
-                                Summary
-                            </Title>
-                            {detailsLoading ? (
-                                <Spinner />
-                            ) : (
-                                <List>
-                                    {/* <ListItem>
+                        {detailsLoading ? (
+                            <Spinner />
+                        ) : (
+                            <List>
+                                {/* <ListItem>
                                         <Text>Type</Text>
                                         <Text className="text-gray-800">?</Text>
                                     </ListItem> */}
-                                    <ListItem>
-                                        <Text>Resources</Text>
-                                        <Text className="text-gray-800">
-                                            {detail?.resource_count}
-                                        </Text>
-                                    </ListItem>
-                                    <ListItem>
-                                        <Text>Connector</Text>
-                                        <Text className="text-gray-800">
-                                            {detail?.connectors}
-                                        </Text>
-                                    </ListItem>
-                                    {/* <ListItem>
+                                <ListItem>
+                                    <Text>Resources</Text>
+                                    <Text className="text-gray-800">
+                                        {detail?.resource_count}
+                                    </Text>
+                                </ListItem>
+                                <ListItem>
+                                    <Text>Connector</Text>
+                                    <Text className="text-gray-800">
+                                        {detail?.connectors}
+                                    </Text>
+                                </ListItem>
+                                {/* <ListItem>
                                         <Text>Services used</Text>
                                         <Text className="text-gray-800">{detail?.resource_count}</Text>
                                     </ListItem> */}
-                                    <ListItem>
-                                        <Text>Status</Text>
-                                        <Text className="text-gray-800">
-                                            {detail?.status}
-                                        </Text>
-                                    </ListItem>
-                                    <ListItem>
-                                        <Text>Last evaluation</Text>
-                                        <Text className="text-gray-800">
-                                            {dateTimeDisplay(
-                                                detail?.last_evaluated_at
-                                            )}
-                                        </Text>
-                                    </ListItem>
-                                    <ListItem>
-                                        <Text>Tags</Text>
-                                        <Flex
-                                            justifyContent="end"
-                                            className="w-2/3 flex-wrap gap-1"
-                                        >
-                                            {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-                                            {/* @ts-ignore */}
-                                            {Object.entries(detail?.tags).map(
-                                                ([name, value]) => (
-                                                    <Tag
-                                                        text={`${name}: ${value}`}
-                                                    />
-                                                )
-                                            )}
-                                        </Flex>
-                                    </ListItem>
-                                </List>
-                            )}
-                        </>
+                                <ListItem>
+                                    <Text>Status</Text>
+                                    <Text className="text-gray-800">
+                                        {detail?.status}
+                                    </Text>
+                                </ListItem>
+                                <ListItem>
+                                    <Text>Last evaluation</Text>
+                                    <Text className="text-gray-800">
+                                        {dateTimeDisplay(
+                                            detail?.last_evaluated_at
+                                        )}
+                                    </Text>
+                                </ListItem>
+                                <ListItem>
+                                    <Text>Tags</Text>
+                                    <Flex
+                                        justifyContent="end"
+                                        className="w-2/3 flex-wrap gap-1"
+                                    >
+                                        {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+                                        {/* @ts-ignore */}
+                                        {Object.entries(detail?.tags).map(
+                                            ([name, value]) => (
+                                                <Tag
+                                                    text={`${name}: ${value}`}
+                                                />
+                                            )
+                                        )}
+                                    </Flex>
+                                </ListItem>
+                            </List>
+                        )}
                         <Flex justifyContent="end">
                             <Button
                                 variant="light"
@@ -271,9 +266,6 @@ export default function ResourceCollectionDetail() {
                     </Flex>
                 </Card>
                 <Card className="h-full">
-                    <Title className="font-semibold mb-2">
-                        Key performance indicator
-                    </Title>
                     <TabGroup className="h-[300px]">
                         <TabList>
                             <Tab>Compliance</Tab>
