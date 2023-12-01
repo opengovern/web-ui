8ff8487520f64eb83646cf8920f2aea4a904a305
Kian AliAkbari <kiana@kaytu.io>
2023-12-01 17:42:48 +0330

    ghost 0 on metric card

--- src/components/Cards/SummaryCard/index.tsx
+++ src/components/Cards/SummaryCard/index.tsx
@@ -97,7 +97,7 @@ export default function SummaryCard({
                         </Text>
                     )}
                 </Flex>
-                {metricPrev && (
+                {!!metricPrev && (
                     <Flex className="mt-1">
                         <ChangeDelta
                             change={
