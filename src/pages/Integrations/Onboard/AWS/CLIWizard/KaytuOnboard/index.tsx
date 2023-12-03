82e1aeb7a12f412ffa098dc1d9ab64e36f688b39
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-12-03 16:33:49 +0400

    fix: Fixes onboard

--- src/pages/Integrations/Onboard/AWS/CLIWizard/KaytuOnboard/index.tsx
+++ src/pages/Integrations/Onboard/AWS/CLIWizard/KaytuOnboard/index.tsx
@@ -1,6 +1,7 @@
 import { Button, Card, Title, Text, Flex, Bold, TextInput } from '@tremor/react'
 import { Checkbox, Switch, useCheckboxState } from 'pretty-checkbox-react'
 import { useEffect, useState } from 'react'
+import { MinusCircleIcon, PlusCircleIcon } from '@heroicons/react/24/outline'
 import { CodeBlock } from '../../../../../../components/CodeBlock'
 
 interface IKaytuOnboard {
@@ -36,34 +37,68 @@ export function KaytuOnboard({
         }
         return txt
     }
+
+    const [openProfile, setOpenProfile] = useState<boolean>(false)
+    const [openOU, setOpenOU] = useState<boolean>(false)
+
     return (
         <Flex flexDirection="col" className="h-full">
             <Flex flexDirection="col" justifyContent="start" alignItems="start">
-                <Text className="mb-2">
-                    Specify your profile in AWS CLI if any
-                </Text>
-                <TextInput
-                    className="mb-4"
-                    onChange={(e) => setProfile(e.target.value)}
-                />
-                {orgOrSingle === 'organization' && (
-                    <>
-                        <Text className="mb-2">
-                            Provide Organization Units (Root OU will be used if
-                            not specified)
-                        </Text>
-                        <TextInput
-                            value={ou}
-                            className="mb-2"
-                            onChange={(e) => setOU(e.target.value)}
-                        />
-                    </>
-                )}
-
-                <Text className="mt-4 mb-2">
-                    Run this command on your terminal
+                <Text className="mb-4 text-gray-900">
+                    Copy and run the command in your terminal
                 </Text>
                 <CodeBlock command={command()} text={`$ ${command()}`} />
+                <Text className="mt-4 mb-4 text-gray-900">Options</Text>
+
+                <Flex
+                    justifyContent="start"
+                    onClick={() => setOpenProfile(!openProfile)}
+                >
+                    {openProfile ? (
+                        <MinusCircleIcon className="text-kaytu-600 w-4" />
+                    ) : (
+                        <PlusCircleIcon className="text-kaytu-600 w-4" />
+                    )}
+                    <Text className="text-kaytu-600 ml-1 mt-1 mb-2">
+                        Specify your AWS CLI profile
+                    </Text>
+                </Flex>
+
+                {openProfile && (
+                    <TextInput
+                        className="mb-4"
+                        placeholder="AWS CLI profile"
+                        onChange={(e) => setProfile(e.target.value)}
+                    />
+                )}
+
+                {orgOrSingle === 'organization' && (
+                    <>
+                        <Flex
+                            justifyContent="start"
+                            className="text-kaytu-600"
+                            onClick={() => setOpenOU(!openOU)}
+                        >
+                            {openOU ? (
+                                <MinusCircleIcon className="text-kaytu-600 w-4" />
+                            ) : (
+                                <PlusCircleIcon className="text-kaytu-600 w-4" />
+                            )}
+                            <Text className="text-kaytu-600 ml-1 mt-1 mb-2">
+                                Specify organization units to onboard
+                            </Text>
+                        </Flex>
+
+                        {openOU && (
+                            <TextInput
+                                value={ou}
+                                className="mb-2"
+                                placeholder="OUs to onboard (separated by comma)"
+                                onChange={(e) => setOU(e.target.value)}
+                            />
+                        )}
+                    </>
+                )}
             </Flex>
             <Flex flexDirection="row" justifyContent="end">
                 <Button variant="secondary" onClick={() => onPrev()}>
