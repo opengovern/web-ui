e7f9a9d5a21bf966497c9dfde7508b4a4b705f65
Kian AliAkbari <kiana@kaytu.io>
2023-12-08 16:23:41 +0330

    some fixes

--- src/pages/Governance/Findings/index.tsx
+++ src/pages/Governance/Findings/index.tsx
@@ -39,6 +39,7 @@ import {
 } from '../../../api/api'
 import FindingDetail from './Detail'
 import { AWSIcon, AzureIcon } from '../../../icons/icons'
+import { renderBadge } from '../Compliance/BenchmarkSummary/Details/Tabs/Policies'
 
 export const columns = (isDemo: boolean) => {
     const temp: IColumn<any, any>[] = [
@@ -152,7 +153,10 @@ export const columns = (isDemo: boolean) => {
             filter: true,
             hide: true,
             resizable: true,
-            flex: 0.5,
+            width: 100,
+            cellRenderer: (param: ValueFormatterParams) => (
+                <Flex className="h-full">{renderBadge(param.value)}</Flex>
+            ),
         },
         {
             field: 'evaluatedAt',
