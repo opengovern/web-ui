c6bb345c6357c72cc3998bc3eccce113c328fbde
Kian AliAkbari <kiana@kaytu.io>
2023-12-13 17:44:13 +0330

    settings fix

--- src/pages/Settings/APIKeys/CreateAPIKey/index.tsx
+++ src/pages/Settings/APIKeys/CreateAPIKey/index.tsx
@@ -13,6 +13,8 @@ import clipboardCopy from 'clipboard-copy'
 import { DocumentDuplicateIcon } from '@heroicons/react/24/outline'
 import InformationModal from '../../../../components/Modal/InformationModal'
 import { useAuthApiV1KeyCreateCreate } from '../../../../api/auth.gen'
+import { useSetAtom } from 'jotai/index'
+import { notificationAtom } from '../../../../store'
 
 interface CreateAPIKeyProps {
     close: () => void
@@ -43,6 +45,7 @@ export default function CreateAPIKey({ close }: CreateAPIKeyProps) {
     const [roleValue, setRoleValue] = useState<
         'admin' | 'editor' | 'viewer' | undefined
     >()
+    const setNotification = useSetAtom(notificationAtom)
 
     const {
         response,
@@ -55,6 +58,22 @@ export default function CreateAPIKey({ close }: CreateAPIKeyProps) {
         {},
         false
     )
+
+    useEffect(() => {
+        if (isExecuted && !isLoading) {
+            setNotification({
+                text: 'API key successfully added',
+                type: 'success',
+            })
+            close()
+        }
+        if (error) {
+            setNotification({
+                text: 'Unable to add new API key',
+                type: 'error',
+            })
+        }
+    }, [isLoading, error])
 
     useEffect(() => {
         if (role === 'viewer' || role === 'editor' || role === 'admin') {
