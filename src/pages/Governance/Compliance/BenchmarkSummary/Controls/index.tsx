b44633a0bc73b8bfd7e1631f030f876b424f8b93
Kian AliAkbari <kiana@kaytu.io>
2023-12-20 16:40:42 +0330

    control list fix

--- src/pages/Governance/Compliance/BenchmarkSummary/Controls/index.tsx
+++ src/pages/Governance/Compliance/BenchmarkSummary/Controls/index.tsx
@@ -23,6 +23,7 @@ import {
     InformationCircleIcon,
 } from '@heroicons/react/24/outline'
 import { useComplianceApiV1BenchmarksControlsDetail } from '../../../../../api/compliance.gen'
+import Spinner from '../../../../../components/Spinner'
 
 interface IPolicies {
     id: string | undefined
@@ -135,127 +136,141 @@ export default function Controls({ id }: IPolicies) {
 
     return (
         <Flex flexDirection="col" className="gap-4">
-            {Object.entries(groupBy(rows(controls), 'parentName'))?.map(
-                ([name, value]: any[]) => (
-                    <Card>
-                        <Accordion
-                            defaultOpen
-                            className="bg-transparent border-0"
-                        >
-                            <AccordionHeader className="pl-0 pr-0.5 py-2 w-full bg-transparent">
-                                <Flex>
+            {isLoading ? (
+                <Spinner className="mt-20" />
+            ) : (
+                Object.entries(groupBy(rows(controls), 'parentName'))?.map(
+                    ([name, value]: any[]) => (
+                        <Card>
+                            <Accordion
+                                defaultOpen
+                                className="bg-transparent border-0"
+                            >
+                                <AccordionHeader className="pl-0 pr-0.5 py-2 w-full bg-transparent">
                                     <Flex>
-                                        <Title className="font-semibold">
-                                            {name}
-                                        </Title>
-                                        <Flex
-                                            justifyContent="start"
-                                            className="gap-2"
-                                            style={{ width: '170px' }}
-                                        >
-                                            {value?.filter((c: any) => c.passed)
-                                                .length === value?.length ? (
-                                                <CheckCircleIcon className="w-5 text-emerald-500" />
-                                            ) : (
-                                                <InformationCircleIcon className="w-5 text-rose-900" />
-                                            )}
-                                            <Text className="font-semibold">{`Passed rules: ${
-                                                value?.filter(
+                                        <Flex>
+                                            <Title className="font-semibold">
+                                                {name}
+                                            </Title>
+                                            <Flex
+                                                justifyContent="start"
+                                                className="gap-2"
+                                                style={{ width: '186px' }}
+                                            >
+                                                {value?.filter(
                                                     (c: any) => c.passed
-                                                ).length
-                                            }/${value?.length}`}</Text>
+                                                ).length === value?.length ? (
+                                                    <CheckCircleIcon className="w-5 text-emerald-500" />
+                                                ) : (
+                                                    <InformationCircleIcon className="w-5 text-rose-900" />
+                                                )}
+                                                <Text className="font-semibold">{`Passed rules: ${
+                                                    value?.filter(
+                                                        (c: any) => c.passed
+                                                    ).length
+                                                }/${
+                                                    value?.length
+                                                } (${Math.round(
+                                                    // eslint-disable-next-line no-unsafe-optional-chaining
+                                                    (value?.filter(
+                                                        (c: any) => c.passed
+                                                    ).length /
+                                                        (value?.length || 0)) *
+                                                        100
+                                                )}%)`}</Text>
+                                            </Flex>
                                         </Flex>
                                     </Flex>
-                                </Flex>
-                            </AccordionHeader>
-                            <AccordionBody className="p-0 pt-4">
-                                <Table className="max-w-full">
-                                    <TableHead className="max-w-full">
-                                        <TableRow className="max-w-full">
-                                            <TableHeaderCell className="w-24">
-                                                Control
-                                            </TableHeaderCell>
-                                            <TableHeaderCell>
-                                                Title
-                                            </TableHeaderCell>
-                                            <TableHeaderCell className="w-40">
-                                                Passed resources
-                                            </TableHeaderCell>
-                                            <TableHeaderCell className="w-5" />
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody className="max-w-full">
-                                        {value.map((v: any, i: number) => (
-                                            <TableRow
-                                                className="max-w-full cursor-pointer hover:bg-kaytu-50"
-                                                key={v?.id}
-                                                onClick={() =>
-                                                    navigate(String(v?.id))
-                                                }
-                                            >
-                                                <TableCell className="w-24 min-w-[96px]">{`${name.substring(
-                                                    0,
-                                                    name.indexOf(' ')
-                                                )}.${i + 1}`}</TableCell>
-                                                <TableCell>
-                                                    <Grid
-                                                        numItems={10}
-                                                        className="w-full"
-                                                    >
-                                                        {severityBadge(
-                                                            v?.severity
-                                                        )}
-                                                        <Col numColSpan={9}>
-                                                            <Text className="truncate">
-                                                                {v?.title}
-                                                            </Text>
-                                                        </Col>
-                                                    </Grid>
-                                                </TableCell>
-                                                <TableCell className="w-40 min-w-[160px]">
-                                                    <Flex
-                                                        justifyContent="start"
-                                                        className="gap-2"
-                                                    >
-                                                        {(v?.totalResourcesCount ||
-                                                            0) -
-                                                            (v?.failedResourcesCount ||
-                                                                0) ===
-                                                        (v?.totalResourcesCount ||
-                                                            0) ? (
-                                                            <CheckCircleIcon className="w-5 text-emerald-500" />
-                                                        ) : (
-                                                            <InformationCircleIcon className="w-5 text-rose-900" />
-                                                        )}
-                                                        {`${
-                                                            (v?.totalResourcesCount ||
-                                                                0) -
-                                                            (v?.failedResourcesCount ||
-                                                                0)
-                                                        }/${
-                                                            v?.totalResourcesCount ||
-                                                            0
-                                                        } (${Math.round(
-                                                            (((v?.totalResourcesCount ||
+                                </AccordionHeader>
+                                <AccordionBody className="p-0 pt-4">
+                                    <Table className="max-w-full">
+                                        <TableHead className="max-w-full">
+                                            <TableRow className="max-w-full">
+                                                <TableHeaderCell className="w-24">
+                                                    Control
+                                                </TableHeaderCell>
+                                                <TableHeaderCell>
+                                                    Title
+                                                </TableHeaderCell>
+                                                <TableHeaderCell className="w-44">
+                                                    Passed resources
+                                                </TableHeaderCell>
+                                                <TableHeaderCell className="w-5" />
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody className="max-w-full">
+                                            {value.map((v: any, i: number) => (
+                                                <TableRow
+                                                    className="max-w-full cursor-pointer hover:bg-kaytu-50"
+                                                    key={v?.id}
+                                                    onClick={() =>
+                                                        navigate(String(v?.id))
+                                                    }
+                                                >
+                                                    <TableCell className="w-24 min-w-[96px]">{`${name.substring(
+                                                        0,
+                                                        name.indexOf(' ')
+                                                    )}.${i + 1}`}</TableCell>
+                                                    <TableCell>
+                                                        <Grid
+                                                            numItems={10}
+                                                            className="w-full"
+                                                        >
+                                                            {severityBadge(
+                                                                v?.severity
+                                                            )}
+                                                            <Col numColSpan={9}>
+                                                                <Text className="truncate">
+                                                                    {v?.title}
+                                                                </Text>
+                                                            </Col>
+                                                        </Grid>
+                                                    </TableCell>
+                                                    <TableCell className="w-44 min-w-[176px]">
+                                                        <Flex
+                                                            justifyContent="start"
+                                                            className="gap-2"
+                                                        >
+                                                            {(v?.totalResourcesCount ||
                                                                 0) -
                                                                 (v?.failedResourcesCount ||
-                                                                    0)) /
+                                                                    0) ===
+                                                            (v?.totalResourcesCount ||
+                                                                0) ? (
+                                                                <CheckCircleIcon className="w-5 text-emerald-500" />
+                                                            ) : (
+                                                                <InformationCircleIcon className="w-5 text-rose-900" />
+                                                            )}
+                                                            {`${
                                                                 (v?.totalResourcesCount ||
-                                                                    1)) *
-                                                                100
-                                                        )}%)`}
-                                                    </Flex>
-                                                </TableCell>
-                                                <TableCell className="w-5 min-w-[20px]">
-                                                    <ChevronRightIcon className="h-5 text-kaytu-500" />
-                                                </TableCell>
-                                            </TableRow>
-                                        ))}
-                                    </TableBody>
-                                </Table>
-                            </AccordionBody>
-                        </Accordion>
-                    </Card>
+                                                                    0) -
+                                                                (v?.failedResourcesCount ||
+                                                                    0)
+                                                            }/${
+                                                                v?.totalResourcesCount ||
+                                                                0
+                                                            } (${Math.round(
+                                                                (((v?.totalResourcesCount ||
+                                                                    0) -
+                                                                    (v?.failedResourcesCount ||
+                                                                        0)) /
+                                                                    (v?.totalResourcesCount ||
+                                                                        1)) *
+                                                                    100
+                                                            )}%)`}
+                                                        </Flex>
+                                                    </TableCell>
+                                                    <TableCell className="w-5 min-w-[20px]">
+                                                        <ChevronRightIcon className="h-5 text-kaytu-500" />
+                                                    </TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </AccordionBody>
+                            </Accordion>
+                        </Card>
+                    )
                 )
             )}
         </Flex>
