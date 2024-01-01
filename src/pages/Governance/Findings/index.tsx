d47cd4ef27a8a9bcfe63c2fe5c0d31eb4aded2b9
Kian AliAkbari <kiana@kaytu.io>
2024-01-01 16:19:33 +0300

    status to findings

--- src/pages/Governance/Findings/index.tsx
+++ src/pages/Governance/Findings/index.tsx
@@ -324,6 +324,8 @@ export default function Findings() {
         sortKey = ''
         setProviderFilter([])
         setProvider('')
+        setStatusFilter(['alarm', 'info', 'skip', 'error'])
+        setStatus(['alarm', 'info', 'skip', 'error'])
         setConnectionFilter([])
         connectionCheckbox.setState([])
         setBenchmarkFilter([])
