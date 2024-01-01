490981f8e539d2954033f75d1645f4b9c9d22095
Kian AliAkbari <kiana@kaytu.io>
2024-01-01 15:13:00 +0300

    .

--- src/api/integration.gen.ts
+++ src/api/integration.gen.ts
@@ -1875,104 +1875,104 @@ interface IuseIntegrationApiV1CredentialDeleteState {
     error?: any
 }
 
-export const useIntegrationApiV1CredentialDelete = (
-    credentialId: string,
-    params: RequestParams = {},
-    autoExecute = true
-) => {
-    const workspace = useParams<{ ws: string }>().ws
-    const [controller, setController] = useState(new AbortController())
-
-    const api = new Api()
-    api.instance = AxiosAPI
-
-    const [state, setState] =
-        useState<IuseIntegrationApiV1CredentialDeleteState>({
-            isLoading: true,
-            isExecuted: false,
-        })
-    const [lastInput, setLastInput] = useState<string>(
-        JSON.stringify([credentialId, params, autoExecute])
-    )
-
-    const sendRequest = (abortCtrl: AbortController) => {
-        if (!api.instance.defaults.headers.common.Authorization) {
-            return
-        }
-
-        setState({
-            ...state,
-            error: undefined,
-            isLoading: true,
-            isExecuted: true,
-        })
-        try {
-            if (workspace !== undefined && workspace.length > 0) {
-                setWorkspace(workspace)
-            } else {
-                setWorkspace('kaytu')
-            }
-
-            const paramsSignal = { ...params, signal: abortCtrl.signal }
-            api.integration
-                .apiV1CredentialDelete(credentialId, paramsSignal)
-                .then((resp) => {
-                    setState({
-                        ...state,
-                        error: undefined,
-                        response: resp.data,
-                        isLoading: false,
-                        isExecuted: true,
-                    })
-                })
-                .catch((err) => {
-                    if (
-                        err.name === 'AbortError' ||
-                        err.name === 'CanceledError'
-                    ) {
-                        // Request was aborted
-                    } else {
-                        setState({
-                            ...state,
-                            error: err,
-                            response: undefined,
-                            isLoading: false,
-                            isExecuted: true,
-                        })
-                    }
-                })
-        } catch (err) {
-            setState({
-                ...state,
-                error: err,
-                isLoading: false,
-                isExecuted: true,
-            })
-        }
-    }
-
-    if (JSON.stringify([credentialId, params, autoExecute]) !== lastInput) {
-        setLastInput(JSON.stringify([credentialId, params, autoExecute]))
-    }
-
-    useEffect(() => {
-        if (autoExecute) {
-            controller.abort()
-            const newController = new AbortController()
-            setController(newController)
-            sendRequest(newController)
-        }
-    }, [lastInput])
-
-    const { response } = state
-    const { isLoading } = state
-    const { isExecuted } = state
-    const { error } = state
-    const sendNow = () => {
-        controller.abort()
-        const newController = new AbortController()
-        setController(newController)
-        sendRequest(newController)
-    }
-    return { response, isLoading, isExecuted, error, sendNow }
-}
+// export const useIntegrationApiV1CredentialDelete = (
+//     credentialId: string,
+//     params: RequestParams = {},
+//     autoExecute = true
+// ) => {
+//     const workspace = useParams<{ ws: string }>().ws
+//     const [controller, setController] = useState(new AbortController())
+//
+//     const api = new Api()
+//     api.instance = AxiosAPI
+//
+//     const [state, setState] =
+//         useState<IuseIntegrationApiV1CredentialDeleteState>({
+//             isLoading: true,
+//             isExecuted: false,
+//         })
+//     const [lastInput, setLastInput] = useState<string>(
+//         JSON.stringify([credentialId, params, autoExecute])
+//     )
+//
+//     const sendRequest = (abortCtrl: AbortController) => {
+//         if (!api.instance.defaults.headers.common.Authorization) {
+//             return
+//         }
+//
+//         setState({
+//             ...state,
+//             error: undefined,
+//             isLoading: true,
+//             isExecuted: true,
+//         })
+//         try {
+//             if (workspace !== undefined && workspace.length > 0) {
+//                 setWorkspace(workspace)
+//             } else {
+//                 setWorkspace('kaytu')
+//             }
+//
+//             const paramsSignal = { ...params, signal: abortCtrl.signal }
+//             api.integration
+//                 .apiV1CredentialDelete(credentialId, paramsSignal)
+//                 .then((resp) => {
+//                     setState({
+//                         ...state,
+//                         error: undefined,
+//                         response: resp.data,
+//                         isLoading: false,
+//                         isExecuted: true,
+//                     })
+//                 })
+//                 .catch((err) => {
+//                     if (
+//                         err.name === 'AbortError' ||
+//                         err.name === 'CanceledError'
+//                     ) {
+//                         // Request was aborted
+//                     } else {
+//                         setState({
+//                             ...state,
+//                             error: err,
+//                             response: undefined,
+//                             isLoading: false,
+//                             isExecuted: true,
+//                         })
+//                     }
+//                 })
+//         } catch (err) {
+//             setState({
+//                 ...state,
+//                 error: err,
+//                 isLoading: false,
+//                 isExecuted: true,
+//             })
+//         }
+//     }
+//
+//     if (JSON.stringify([credentialId, params, autoExecute]) !== lastInput) {
+//         setLastInput(JSON.stringify([credentialId, params, autoExecute]))
+//     }
+//
+//     useEffect(() => {
+//         if (autoExecute) {
+//             controller.abort()
+//             const newController = new AbortController()
+//             setController(newController)
+//             sendRequest(newController)
+//         }
+//     }, [lastInput])
+//
+//     const { response } = state
+//     const { isLoading } = state
+//     const { isExecuted } = state
+//     const { error } = state
+//     const sendNow = () => {
+//         controller.abort()
+//         const newController = new AbortController()
+//         setController(newController)
+//         sendRequest(newController)
+//     }
+//     return { response, isLoading, isExecuted, error, sendNow }
+// }
