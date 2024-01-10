83ba3a1f3c0d3643bbf836268b34b9b523e82615
Kian AliAkbari <kiana@kaytu.io>
2024-01-10 17:34:18 +0300

    queries

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,30 @@
-import { Col, Grid } from '@tremor/react'
+import { Col, Flex, Grid, Title } from '@tremor/react'
 import Layout from '../../components/Layout'
 import QuickNav from './QuickNav'
-import TopSpend from './TopSpend'
 import Integration from './Integration'
 import Governance from './Governance'
+import Query from './Query'
 
 export default function Home() {
+    const element = document.getElementById('myDIV')?.offsetHeight
     return (
         <Layout currentPage="home">
-            <Grid numItems={6} className="w-full gap-4">
+            <Grid numItems={6} className="w-full gap-4 h-fit">
                 <Col numColSpan={4}>
-                    <QuickNav />
-                    <Grid numItems={3} className="w-full gap-4 mb-4">
-                        <Integration />
-                    </Grid>
-                    <Governance />
+                    <Title className="font-semibold mb-4">
+                        Quick navigation
+                    </Title>
+                    <Flex flexDirection="col" alignItems="start" id="myDIV">
+                        <QuickNav />
+                        <Grid numItems={3} className="w-full gap-4 mb-4">
+                            <Integration />
+                        </Grid>
+                        <Governance />
+                    </Flex>
                 </Col>
-                <Col numColSpan={2}>query</Col>
+                <Col numColSpan={2}>
+                    <Query height={element} />
+                </Col>
             </Grid>
         </Layout>
     )
