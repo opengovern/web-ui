58a1c351a8330f180a444376f29d4d48501d725a
Kian AliAkbari <kiana@kaytu.io>
2024-01-10 15:57:56 +0300

    findings

---
+++ src/pages/Home/Governance/index.tsx
@@ -0,0 +1,21 @@
+import { Card, Col, Flex, Grid, Icon, Title } from '@tremor/react'
+import { ShieldCheckIcon } from '@heroicons/react/24/outline'
+import Compliance from '../Compliance'
+import Findings from '../Findings'
+
+export default function Governance() {
+    return (
+        <Card>
+            <Flex justifyContent="start" className="mb-4">
+                <Icon icon={ShieldCheckIcon} />
+                <Title>Governance</Title>
+            </Flex>
+            <Grid numItems={3} className="w-full gap-4">
+                <Compliance />
+                <Col numColSpan={2}>
+                    <Findings />
+                </Col>
+            </Grid>
+        </Card>
+    )
+}
