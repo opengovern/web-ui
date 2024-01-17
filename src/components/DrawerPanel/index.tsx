e93ee766ed72377a91b16abf6e2fcb2460457b71
Kian AliAkbari <kiana@kaytu.io>
2024-01-18 00:00:35 +0300

    .

--- src/components/DrawerPanel/index.tsx
+++ src/components/DrawerPanel/index.tsx
@@ -49,7 +49,7 @@ export default function DrawerPanel({
                                         justifyContent="start"
                                         className="h-full w-full bg-white dark:bg-gray-900 py-8 shadow-xl border-l border-l-gray-700"
                                     >
-                                        <Dialog.Title className="absolute top-0 z-10 w-full bg-white dark:bg-gray-900 px-6 border-b border-b-gray-700 pt-4 pb-4">
+                                        <Dialog.Title className="absolute top-0 z-10 w-full bg-white dark:bg-gray-900 px-6 border-b dark:border-b-gray-700 pt-4 pb-4">
                                             <Flex>
                                                 {typeof title === 'string' ? (
                                                     <Title className="text-lg font-semibold">
