32dd547a65a326156b2d56ab9fc59449a3d753f5
Kian AliAkbari <kiana@kaytu.io>
2024-01-30 19:53:33 +0400

    timeline fix

--- src/pages/Governance/Findings/FindingsWithFailure/Detail/Timeline/index.tsx
+++ src/pages/Governance/Findings/FindingsWithFailure/Detail/Timeline/index.tsx
@@ -1,9 +1,25 @@
 import { Flex, Icon, Text, Title } from '@tremor/react'
-import { XCircleIcon } from '@heroicons/react/24/solid'
+import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/24/solid'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import dayjs from 'dayjs'
 import { severityBadge } from '../../../../Controls'
+import { GithubComKaytuIoKaytuEnginePkgComplianceApiGetSingleResourceFindingResponse } from '../../../../../../api/api'
+import Spinner from '../../../../../../components/Spinner'
+import { dateDisplay } from '../../../../../../utilities/dateDisplay'
 
-export default function Timeline() {
-    return (
+dayjs.extend(relativeTime)
+
+interface ITimeline {
+    data:
+        | GithubComKaytuIoKaytuEnginePkgComplianceApiGetSingleResourceFindingResponse
+        | undefined
+    isLoading: boolean
+}
+
+export default function Timeline({ data, isLoading }: ITimeline) {
+    return isLoading ? (
+        <Spinner />
+    ) : (
         <Flex
             flexDirection="col"
             justifyContent="start"
@@ -14,90 +30,45 @@ export default function Timeline() {
                 className="absolute w-0.5 h-full bg-gray-200 z-10 top-1"
                 style={{ left: 'calc(20% + 51px)' }}
             />
-            <Flex alignItems="start" className="gap-6 z-20">
-                <Flex flexDirection="col" alignItems="end" className="w-1/3">
-                    <Title>Time</Title>
-                    <Text>Time</Text>
-                </Flex>
-                <Icon
-                    icon={XCircleIcon}
-                    color="rose"
-                    size="xl"
-                    className="p-0"
-                />
-                <Flex flexDirection="col" alignItems="start" className="gap-1">
-                    <Title>Some random title</Title>
-                    <Flex className="w-fit gap-4">
-                        {severityBadge('critical')}
-                        <Text className="pl-4 border-l border-l-gray-200">
-                            Section:
-                        </Text>
+            {data?.findingEvents?.map((tl) => (
+                <Flex alignItems="start" className="gap-6 z-20">
+                    <Flex
+                        flexDirection="col"
+                        alignItems="end"
+                        className="w-1/3"
+                    >
+                        <Title>{dateDisplay(tl.evaluatedAt)}</Title>
+                        <Text>{dayjs(tl?.evaluatedAt).fromNow()}</Text>
+                    </Flex>
+                    <Icon
+                        icon={
+                            tl.conformanceStatus === 'failed'
+                                ? XCircleIcon
+                                : CheckCircleIcon
+                        }
+                        color={
+                            tl.conformanceStatus === 'failed'
+                                ? 'rose'
+                                : 'emerald'
+                        }
+                        size="xl"
+                        className="p-0"
+                    />
+                    <Flex
+                        flexDirection="col"
+                        alignItems="start"
+                        className="gap-1"
+                    >
+                        <Title>{tl.controlID}</Title>
+                        <Flex className="w-fit gap-4">
+                            {severityBadge(tl.severity)}
+                            <Text className="pl-4 border-l border-l-gray-200">
+                                Section:
+                            </Text>
+                        </Flex>
                     </Flex>
                 </Flex>
-            </Flex>
-            <Flex alignItems="start" className="gap-6 z-20">
-                <Flex flexDirection="col" alignItems="end" className="w-1/3">
-                    <Title>Time</Title>
-                    <Text>Time</Text>
-                </Flex>
-                <Icon
-                    icon={XCircleIcon}
-                    color="rose"
-                    size="xl"
-                    className="p-0"
-                />
-                <Flex flexDirection="col" alignItems="start" className="gap-1">
-                    <Title>Some random title</Title>
-                    <Flex className="w-fit gap-4">
-                        {severityBadge('critical')}
-                        <Text className="pl-4 border-l border-l-gray-200">
-                            Section:
-                        </Text>
-                    </Flex>
-                </Flex>
-            </Flex>
-            <Flex alignItems="start" className="gap-6 z-20">
-                <Flex flexDirection="col" alignItems="end" className="w-1/3">
-                    <Title>Time</Title>
-                    <Text>Time</Text>
-                </Flex>
-                <Icon
-                    icon={XCircleIcon}
-                    color="rose"
-                    size="xl"
-                    className="p-0"
-                />
-                <Flex flexDirection="col" alignItems="start" className="gap-1">
-                    <Title>Some random title</Title>
-                    <Flex className="w-fit gap-4">
-                        {severityBadge('critical')}
-                        <Text className="pl-4 border-l border-l-gray-200">
-                            Section:
-                        </Text>
-                    </Flex>
-                </Flex>
-            </Flex>
-            <Flex alignItems="start" className="gap-6 z-20">
-                <Flex flexDirection="col" alignItems="end" className="w-1/3">
-                    <Title>Time</Title>
-                    <Text>Time</Text>
-                </Flex>
-                <Icon
-                    icon={XCircleIcon}
-                    color="rose"
-                    size="xl"
-                    className="p-0"
-                />
-                <Flex flexDirection="col" alignItems="start" className="gap-1">
-                    <Title>Some random title</Title>
-                    <Flex className="w-fit gap-4">
-                        {severityBadge('critical')}
-                        <Text className="pl-4 border-l border-l-gray-200">
-                            Section:
-                        </Text>
-                    </Flex>
-                </Flex>
-            </Flex>
+            ))}
         </Flex>
     )
 }
