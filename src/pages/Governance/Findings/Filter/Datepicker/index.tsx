e67a932f669be40ab561afdb4ff19256b507a66e
Kian AliAkbari <kiana@kaytu.io>
2024-01-31 16:33:45 +0400

    .

--- src/pages/Governance/Findings/Filter/Datepicker/index.tsx
+++ src/pages/Governance/Findings/Filter/Datepicker/index.tsx
@@ -1,3 +1,58 @@
+import { getLocalTimeZone, parseDate, today } from '@internationalized/date'
+import dayjs from 'dayjs'
+import { AriaDateRangePickerProps, DateValue } from '@react-aria/datepicker'
+import { useDateRangePickerState } from 'react-stately'
+import { useRef } from 'react'
+import { useDateRangePicker } from 'react-aria'
+import {
+    defaultTime,
+    useUrlDateRangeState,
+} from '../../../../../utilities/urlstate'
+import { RangeCalendar } from '../../../../../components/Layout/Header/DateRangePicker/Calendar/RangeCalendar'
+
+function CustomDatePicker(props: AriaDateRangePickerProps<DateValue>) {
+    const state = useDateRangePickerState(props)
+    const ref = useRef(null)
+
+    const { groupProps, labelProps, buttonProps, dialogProps, calendarProps } =
+        useDateRangePicker(props, state, ref)
+
+    return <RangeCalendar {...calendarProps} />
+}
+
 export default function Datepicker() {
-    return <div>hi</div>
+    const { value: activeTimeRange, setValue: setActiveTimeRange } =
+        useUrlDateRangeState(defaultTime)
+
+    const currentValue = () => {
+        return {
+            start: parseDate(
+                activeTimeRange.start.startOf('day').format('YYYY-MM-DD')
+            ),
+            end: parseDate(
+                activeTimeRange.end.endOf('day').format('YYYY-MM-DD')
+            ),
+        }
+    }
+
+    const minValue = () => {
+        return parseDate('2022-12-01')
+    }
+    const maxValue = () => {
+        return today(getLocalTimeZone())
+    }
+
+    return (
+        <CustomDatePicker
+            value={currentValue()}
+            onChange={(value) => {
+                setActiveTimeRange({
+                    start: dayjs.utc(value.start.toString()).startOf('day'),
+                    end: dayjs.utc(value.end.toString()).endOf('day'),
+                })
+            }}
+            minValue={minValue()}
+            maxValue={maxValue()}
+        />
+    )
 }
--- src/pages/Governance/Findings/Filter/index.tsx
+++ src/pages/Governance/Findings/Filter/index.tsx
@@ -30,6 +30,7 @@ import {
     SeverityIcon,
 } from '../../../../icons/icons'
 import Severity from './Severity'
+import Datepicker from './Datepicker'
 
 const removeItem = (arr: any[], value: any) => {
     const index = arr.indexOf(value)
@@ -367,6 +368,24 @@ export default function Filter({ onApply }: IFilters) {
     return (
         <Flex justifyContent="start" className="mt-4 gap-3 flex-wrap z-10">
             {renderFilters}
+            <Popover className="relative border-0">
+                <Popover.Button>date</Popover.Button>
+                <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
+                >
+                    <Popover.Panel className="absolute z-50 top-full left-0">
+                        <Card className="mt-2 p-4 w-fit">
+                            <Datepicker />
+                        </Card>
+                    </Popover.Panel>
+                </Transition>
+            </Popover>
             {filterOptions.filter((f) => !selectedFilters.includes(f.id))
                 .length > 0 && (
                 <Flex className="w-fit pl-3 border-l border-l-gray-200 h-full">
