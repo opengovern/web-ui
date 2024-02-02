0291eb1694b9f31cd814976f47d836ec1f80b619
Kian AliAkbari <kiana@kaytu.io>
2024-02-02 15:24:08 +0400

    some fixes

--- src/pages/Governance/Findings/Events/Detail/index.tsx
+++ src/pages/Governance/Findings/Events/Detail/index.tsx
@@ -4,25 +4,21 @@ import { getConnectorIcon } from '../../../../../components/Cards/ConnectorCard'
 import DrawerPanel from '../../../../../components/DrawerPanel'
 
 interface IFindingDetail {
-    finding: GithubComKaytuIoKaytuEnginePkgComplianceApiFindingEvent | undefined
+    event: GithubComKaytuIoKaytuEnginePkgComplianceApiFindingEvent | undefined
     open: boolean
     onClose: () => void
 }
 
-export default function EventDetail({
-    finding,
-    open,
-    onClose,
-}: IFindingDetail) {
+export default function EventDetail({ event, open, onClose }: IFindingDetail) {
     return (
         <DrawerPanel
             open={open}
             onClose={onClose}
             title={
                 <Flex justifyContent="start">
-                    {getConnectorIcon(finding?.connector)}
+                    {getConnectorIcon(event?.connector)}
                     <Title className="text-lg font-semibold ml-2 my-1">
-                        {finding?.id}
+                        {event?.providerConnectionName}
                     </Title>
                 </Flex>
             }
