5b98fa29ee2a781de6bc2b206a8eb09a3f65edcc
Saleh Khazaei <saleh.khazaei@gmail.com>
2024-02-14 16:49:34 +0400

    fix: Fixes reason break

--- src/pages/Governance/Findings/FindingsWithFailure/Detail/index.tsx
+++ src/pages/Governance/Findings/FindingsWithFailure/Detail/index.tsx
@@ -258,7 +258,7 @@ export default function FindingDetail({
                                         className="w-full"
                                     >
                                         <Text className="w-1/4">Reason</Text>
-                                        <Text className="text-gray-800 text-end w-3/4 whitespace-break-spaces">
+                                        <Text className="text-gray-800 text-end w-3/4 whitespace-break-spaces h-fit">
                                             {finding?.reason}
                                         </Text>
                                     </Flex>
