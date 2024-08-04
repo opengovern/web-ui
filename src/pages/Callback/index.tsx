6c3eb1d1b3200df4801666b0c61e84e8cc3911fa
Saleh Khazaei <saleh.khazaei@gmail.com>
2024-08-04 19:28:45 +0400

    fix: Fixes error description

--- src/pages/Callback/index.tsx
+++ src/pages/Callback/index.tsx
@@ -1,10 +1,17 @@
+import { useSearchParams } from 'react-router-dom'
 import { useAuth } from '../../utilities/auth'
 
 export const CallbackPage = () => {
+    const [locationSearchParams, setSearchParams] = useSearchParams()
+
     const { error, isAuthenticated } = useAuth()
     if (isAuthenticated) {
         window.location.href = '/'
         return null
+    }
+
+    if (locationSearchParams.has('error_description')) {
+        return <span>{locationSearchParams.get('error_description')}</span>
     }
 
     if (error) {
